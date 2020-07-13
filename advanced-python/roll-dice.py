d952a7ecf2f20637239eb9a882c851568199ec75
Dan McCreary <dan.mccreary@gmail.com>
2020-07-13 14:27:46 -0600

    Deployed 1f49008 with MkDocs version: 1.1.2

---
+++ advanced-python/roll-dice.py
@@ -0,0 +1,11 @@
+import random
+min = 1
+max = 6
+
+roll_again = "yes"
+
+while roll_again == "yes" or roll_again == "y":
+    print("Rolling the dice.  The values are:")
+    print(random.randint(min, max))
+    print(random.randint(min, max))
+    roll_again = input("Roll the dices again?")
